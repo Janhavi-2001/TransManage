e6ce7d3983f8e4fb60a4f517bf761e8b605c2553
Janhavi-2001 <jpimplikar26@gmail.com>
2025-06-10 22:32:03 +0530

    Add loader component

--- TransManage_frontend/src/App.js
+++ TransManage_frontend/src/App.js
@@ -1,16 +1,32 @@
+import React, { useEffect, useState } from 'react';
 import Homepage from './Components/Homepage/Homepage';
 import Navbar from './Components/Navbar/Navbar';
+import Loader from './Components/Loader/Loader';
 import './App.css';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-  <div className="App">
-    <div className="container">
-      <Navbar />
-        <Homepage />
+    <div className="App">
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className="container">
+          <Navbar />
+          <Homepage />
+        </div>
+      )}
     </div>
-  </div>
-);
+  );
 }
 
 export default App;
---
+++ TransManage_frontend/src/Components/Loader/Loader.js
@@ -0,0 +1,16 @@
+import './Loader.css';
+
+const Loader = () => {
+  
+  return (
+    <div className="loader">
+      <div className="spinner">
+        <div className="bounce1"></div>
+        <div className="bounce2"></div>
+        <div className="bounce3"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
